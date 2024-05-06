8aecab79264323fa53766f40275ec96ed55c3007
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-05-07 01:10:51 +0300

    init commit

---
+++ spy.mjs
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+import HcLibSpy from './util.mjs';
+
+const PACKAGES = [
+	'@holochain/client',
+	'@holochain/tryorama',
+	'@holochain/hc-spin',
+	'@holo-host/web-sdk'
+];
+
+const releasedPackageToVersion = {};
+
+function formatMapping(map) {
+	let formattedString = '';
+	for (let [key, value] of Object.entries(map)) {
+			formattedString += `${key}: ${value}\n`;
+	}
+	return formattedString.trim();
+}
+
+;(async () => {
+	for (let p of PACKAGES) {
+		const spy = new HcLibSpy(p);
+		const release = await spy.checkMostRecentRelease();
+		if (release) {
+			releasedPackageToVersion[p] = release; 
+		}
+	}
+	console.log(formatMapping(releasedPackageToVersion));
+})();
