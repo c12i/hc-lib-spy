28e066874bd996a78f65bd03fe415b73a23c6261
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-11-26 18:43:25 +0300

    Update fetch script

--- spy.mjs
+++ spy.mjs
@@ -1,27 +1,27 @@
 #!/usr/bin/env node
 
-import HcLibSpy from './util.mjs';
+import HcLibSpy from "./util.mjs";
 
 const PACKAGES = [
-	'@holochain/client',
-	'@holochain/tryorama',
-	'@holochain/hc-spin',
-	'@holo-host/web-sdk',
-	'@holochain-playground/cli'
+  "@holochain/client",
+  "@holochain/tryorama",
+  "@holochain/hc-spin",
+  "@holo-host/web-sdk",
+  "@holochain-playground/cli",
 ];
 
 const releasedPackageToVersion = {};
 
 (async () => {
-	for (let p of PACKAGES) {
-		const spy = new HcLibSpy(p);
-		const release = await spy.checkMostRecentRelease();
-		if (release) {
-			releasedPackageToVersion[p] = release;
-		}
-	}
-	const result = JSON.stringify(releasedPackageToVersion);
-	if (result !== '{}') {
-		console.log(JSON.stringify(releasedPackageToVersion));
-	}
+  for (let p of PACKAGES) {
+    const spy = new HcLibSpy(p);
+    const release = await spy.checkMostRecentRelease();
+    if (release) {
+      releasedPackageToVersion[p] = release;
+    }
+  }
+  const result = JSON.stringify(releasedPackageToVersion);
+  if (result !== "{}") {
+    console.log(JSON.stringify(releasedPackageToVersion));
+  }
 })();
--- util.mjs
+++ util.mjs
@@ -1,57 +1,64 @@
-import https from 'node:https'
+import https from "node:https";
 
 function offsetNHours(n) {
-	return new Date(new Date().getTime() - (n * 60 * 60 * 1000));
+  return new Date(new Date().getTime() - n * 60 * 60 * 1000);
 }
 
 class HcLibSpy {
-	constructor(_package) {
-		this.package = _package;
-	}
+  constructor(_package) {
+    this.package = _package;
+  }
 
-	async checkMostRecentRelease(time = 6) {
-		const offsetN = offsetNHours(time);
-		const result = await this._fetchPackageDetails();
-		if (!(result && 'time' in result)) {
-			throw new Error('Failed to check most recent release: Invalid response body');
-		}
-		const releasesAndTimestamp = Object.entries(result.time);
-		const mostRecentRelease = releasesAndTimestamp
-			.filter(([_, timestamp]) => new Date(timestamp) >= offsetN)
-			.filter(([version]) => !['created', 'modified'].includes(version))
-			.sort(([_, timestampA], [__, timestampB]) => new Date(timestampB) - new Date(timestampA))
-			.map(([version]) => version)
-		if (!mostRecentRelease.length) {
-			return;
-		}
-		return mostRecentRelease[0]
-	}
+  async checkMostRecentRelease(time = 6) {
+    const offsetN = offsetNHours(time);
+    const result = await this._fetchPackageDetails();
+    if (!(result && "time" in result)) {
+      throw new Error(
+        "Failed to check most recent release: Invalid response body",
+      );
+    }
+    const releasesAndTimestamp = Object.entries(result.time);
+    const mostRecentRelease = releasesAndTimestamp
+      .filter(([_, timestamp]) => new Date(timestamp) >= offsetN)
+      .filter(([version]) => !["created", "modified"].includes(version))
+      .sort(
+        ([_, timestampA], [__, timestampB]) =>
+          new Date(timestampB) - new Date(timestampA),
+      )
+      .map(([version]) => version);
+    if (!mostRecentRelease.length) {
+      return;
+    }
+    return mostRecentRelease[0];
+  }
 
-	_fetchPackageDetails() {
-		const URL = `https://registry.npmjs.org/${this.package}`;
-		const request = {
-			headers: { 'User-Agent': 'hc-lib-spy' }
-		};
-		return new Promise((resolve, reject) => {
-			https.get(URL, request, (res) => {
-				let data = '';
-				res.on('data', (chunk) => {
-					data += chunk;
-				});
-				res.on('end', () => {
-					try {
-						resolve(JSON.parse(data))
-					} catch (e) {
-						console.error('Error processing result', e);
-						resolve(e);
-					}
-				});
-			}).on('error', (e) => {
-				console.error(`Got error: ${e.message}`);
-				resolve(e);
-			});
-		});
-	}
+  _fetchPackageDetails() {
+    const URL = `https://registry.npmjs.org/${this.package}`;
+    const request = {
+      headers: { "User-Agent": "hc-lib-spy" },
+    };
+    return new Promise((resolve, reject) => {
+      https
+        .get(URL, request, (res) => {
+          let data = "";
+          res.on("data", (chunk) => {
+            data += chunk;
+          });
+          res.on("end", () => {
+            try {
+              resolve(JSON.parse(data));
+            } catch (e) {
+              console.error("Error processing result", e);
+              resolve(e);
+            }
+          });
+        })
+        .on("error", (e) => {
+          console.error(`Got error: ${e.message}`);
+          reject(e);
+        });
+    });
+  }
 }
 
 export default HcLibSpy;
